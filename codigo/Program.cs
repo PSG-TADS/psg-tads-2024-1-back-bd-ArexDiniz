99a71f6cafda5c81a100dfdb9ff2c4a852744aa0
AR3X <alexgdiniz18@gmail.com>
2024-04-19 17:18:05 -0300

    Criando Entidades

--- codigo/Program.cs
+++ codigo/Program.cs
@@ -1,8 +1,60 @@
 
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
 namespace codigo
 {
     public class Program
     {
+        public class Veiculo
+        {
+            [Key]
+            public int VeiculoID { get; set; }
+            public string Marca { get; set; }
+            public string Modelo { get; set; }
+            public int Ano { get; set; }
+            public float ValorDiaria { get; set; }
+
+            public Boolean Reservado { get; set; }
+        }
+
+        public class Cliente
+        {
+            [Key]
+            public int ClienteID { get; set; }
+            public string Nome { get; set; }
+            public string Endereco { get; set; }
+            public string Telefone { get; set; }
+        }
+
+        public class Reserva
+        {
+            [Key]
+            public int ReservaID { get; set; }
+            public int VeiculoID { get; set; }
+            [ForeignKey("VeiculoID")]
+            public int ClienteID { get; set; }
+            [ForeignKey("ClienteID")]
+            public DateTime DataInicio { get; set; }
+            public DateTime DataFim { get; set; }
+
+
+            public Veiculo Veiculo { get; set; }
+
+            public virtual Cliente? Cliente { get; set; }
+        }
+        public class LocadoraContext : DbContext
+        {
+            public DbSet<Veiculo> Veiculos { get; set; }
+            public DbSet<Cliente> Clientes { get; set; }
+            public DbSet<Reserva> Reservas { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                _ = optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESS ; Database=Locadora;Trusted_Connection=True;TrustServerCertificate=true");
+            }
+        }
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
